ea00894f1e05e02524cb74ed7e68acd60369f76e
Yangju1009 <145637278+yangju1009@users.noreply.github.com>
2024-03-12 17:55:41 +0900

    프로그래머스 코테 기초 문제

---
+++ programmers/level1/pccp기출문제1번/Solution.java
@@ -0,0 +1,15 @@
+package pccp기출문제1번;
+
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int life = health; 
+		/* int lastTime = attacks[]; */
+        for (int i = 0; i < attacks.length; i++) {
+			
+		}
+       
+        
+        return life;
+        
+    }
+}
