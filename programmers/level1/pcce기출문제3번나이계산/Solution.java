2621603e96192d33c1374b07c7342be794944208
Yangju1009 <145637278+yangju1009@users.noreply.github.com>
2024-03-12 20:36:08 +0900

    03.12 코딩테스트 연습

---
+++ programmers/level1/pcce기출문제3번나이계산/Solution.java
@@ -0,0 +1,15 @@
+package pcce기출문제3번나이계산;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+        System.out.println(b_square);
+    }
+}
--- programmers/level1/pccp기출문제1번/Solution.java
+++ programmers/level1/pccp기출문제1번/Solution.java
@@ -3,7 +3,8 @@ package pccp기출문제1번;
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
         int life = health; 
-		/* int lastTime = attacks[]; */
+		int attackInfo =  0;
+		int heal = 0;
         for (int i = 0; i < attacks.length; i++) {
 			
 		}
