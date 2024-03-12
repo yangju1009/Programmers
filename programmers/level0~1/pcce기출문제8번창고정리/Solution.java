7739b2eb1df6fdb861c10fb65c2ba30841716ce1
Yangju1009 <145637278+yangju1009@users.noreply.github.com>
2024-03-12 23:19:50 +0900

    3.12 pcce 기출문제

---
+++ programmers/level0~1/pcce기출문제8번창고정리/Solution.java
@@ -0,0 +1,51 @@
+package pcce기출문제8번창고정리;
+
+/*문제 설명
+선빈이는 게임을 즐기던 중 가지고 있는 물건이 너무 많아 창고 정리를 하기로 했습니다. 
+선빈이가 보유한 게임 속 창고는 여러 칸으로 나누어져 있고 각 칸에는 물건들이 담겨있습니다. 
+창고를 정리할 방법을 고민하던 선빈이는 같은 물건이 여러 칸에 나누어 들어있는 것을 발견하고 우선 같은 물건끼리 최대한 겹쳐쌓는 방식으로 창고를 정리하기로 했습니다. 
+선빈이의 창고에 들어있는 물건의 이름과 개수는 리스트 형태로 주어지며, 한 칸에 겹쳐질 수 있는 물건의 개수에는 제한이 없다고 가정합니다.*/
+
+/* 예를 들어 창고의 
+각 칸에 담겨있는 물건의 이름이 storage = ["pencil", "pencil", "pencil", "book"], 
+각 물건의 개수가 num = [2, 4, 3, 1]이라면 
+연필과 책을 한 칸에 각각 겹쳐 쌓아 간단하게 clean_storage = ["pencil", "book"],
+clean_num = [9, 1]로 만들 수 있습니다.*/
+class Solution {
+	public String solution(String[] storage, int[] num) {
+		int num_item = 0;
+		String[] clean_storage = new String[storage.length];
+		int[] clean_num = new int[num.length];
+
+		for (int i = 0; i < storage.length; i++) {
+			int clean_idx = -1;
+			for (int j = 0; j < num_item; j++) {
+				if (storage[i].equals(clean_storage[j])) {
+					clean_idx = j;
+					break;
+				}
+			}
+			if (clean_idx == -1) {
+				/* clean_storage[num_item] = Integer.toString(num[i]); */
+				/* 물건의 개수를 입력하는게 아니라 물건의 이름을 입력해야함. 따라서 수정 필요 */
+				clean_storage[num_item] = storage[i];
+				clean_num[num_item] = num[i];
+				num_item += 1;
+			} else {
+				clean_num[clean_idx] += num[i];
+			}
+		}
+
+		// 아래 코드에는 틀린 부분이 없습니다.
+
+		int num_max = -1;
+		String answer = "";
+		for (int i = 0; i < num_item; i++) {
+			if (clean_num[i] > num_max) {
+				num_max = clean_num[i];
+				answer = clean_storage[i];
+			}
+		}
+		return answer;
+	}
+}
---
+++ programmers/level0~1/pccp기출문제1번/Solution.java
@@ -0,0 +1,16 @@
+package pccp기출문제1번;
+
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int life = health; 
+		int attackInfo =  0;
+		int heal = 0;
+        for (int i = 0; i < attacks.length; i++) {
+			
+		}
+       
+        
+        return life;
+        
+    }
+}
